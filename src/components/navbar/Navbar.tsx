81253fddb5f3cda667f424db0651c16b3b469055
kartik suthar <kartiksuthar2756@gmail.com>
2025-01-31 23:32:16 +0530

    asd

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-export const Navbar = () => {
+export const Navbar = ({ isauth }) => {
   const { status } = useSession();
 
   const router = useRouter();
@@ -26,7 +26,7 @@ export const Navbar = () => {
           >
             Loading...
           </button>
-        ) : status === "authenticated" ? (
+        ) : isauth ? (
           <button
             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
             onClick={() => router.push("/auth/signin")}
