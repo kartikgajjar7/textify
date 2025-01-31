954bccd780299ff0e252a6d82200b96db8c3545a
kartik suthar <kartiksuthar2756@gmail.com>
2025-01-31 21:07:24 +0530

    done

--- schema/index.ts
+++ schema/index.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email("Please enter a valid email address"),
-  password: z.string(),
+  password: z.string().min(1, "Password cannot be empty"),
 });
 export const registerSchema = z.object({
   email: z.string().email("Please enter a valid email address"),
