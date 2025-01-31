5451620b04b8e0e96b03b317b44784ffda18fb38
kartik suthar <kartiksuthar2756@gmail.com>
2025-01-31 23:02:00 +0530

    ding

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,11 +2,9 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-export const Navbar = ({ isauth }) => {
+export const Navbar = () => {
   const { status } = useSession();
-  const isAuthenticated = status === "authenticated";
 
-  console.log(isAuthenticated, "ISAUTH");
   const router = useRouter();
   const handleSignIn = () => {
     router.push("/auth/signin"); // Navigate to /auth/signin
@@ -21,17 +19,24 @@ export const Navbar = ({ isauth }) => {
           width={100}
           height={100}
         />
-        {isAuthenticated ? (
+        {status === "loading" ? (
           <button
             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-            onClick={() => router.push("/dashboard")}
+            onClick={() => router.push("/auth/signin")}
+          >
+            Loading...
+          </button>
+        ) : status === "authenticated" ? (
+          <button
+            className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            onClick={() => router.push("/auth/signin")}
           >
             Dashboard
           </button>
         ) : (
           <button
             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-            onClick={handleSignIn}
+            onClick={() => router.push("/auth/signin")}
           >
             Sign In
           </button>
