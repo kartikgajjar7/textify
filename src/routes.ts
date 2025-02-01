8ce44ecb68fe5a5053a78ad268cf814ba5d42e75
kartik suthar <kartiksuthar2756@gmail.com>
2025-02-01 20:14:31 +0530

    dping

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,11 @@
 export const publickRoutes = ["/"];
-export const authRoutes = ["/auth/signin", "/auth/signup"];
+export const authRoutes = [
+  "/auth/signin",
+  "/auth/signup",
+  "dashboard",
+  "dashboard/setting",
+  "dashboard/profile",
+  "dashboard/analytics",
+];
 export const apiAuthprefix = "/api/auth";
 export const DEFAULT_REDIRECT = "/dashboard";
