77327e871d8bedc41f2d838e642b48f16169269b
kartik suthar <kartiksuthar2756@gmail.com>
2025-02-01 20:42:13 +0530

    asd

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,9 @@
-export const publickRoutes = ["/dashboard", "/"];
+export const publickRoutes = [
+  "/dashboard",
+  "/dashboard/analytics",
+  "dashboard/settings",
+  "dashboard/profile",
+];
 export const authRoutes = ["/auth/signin", "/auth/signup"];
 export const apiAuthprefix = "/api/auth";
 export const DEFAULT_REDIRECT = "/dashboard";
