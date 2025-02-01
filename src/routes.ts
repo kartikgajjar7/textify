40859e30e6a29e77e753dd841f93f7329d542452
kartik suthar <kartiksuthar2756@gmail.com>
2025-02-01 20:21:52 +0530

    try

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,8 @@
-export const publickRoutes = ["/"];
-export const authRoutes = [
-  "/auth/signin",
-  "/auth/signup",
-  "dashboard",
-  "dashboard/setting",
-  "dashboard/profile",
-  "dashboard/analytics",
+export const publickRoutes = [
+  "/dashboard/setting",
+  "/dashboard/analytics",
+  "/dashboard/profile",
 ];
+export const authRoutes = ["/auth/signin", "/auth/signup"];
 export const apiAuthprefix = "/api/auth";
 export const DEFAULT_REDIRECT = "/dashboard";
